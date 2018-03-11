fc377a9c7e65430df06aff9a0e89d7ea6d24c921
jinxin <jinxin_hz@163.com>
2018-03-11 16:15:25 +0800

    first idea change

---
+++ demo01/src/com/jinxin/demo01/Demo01.java
@@ -0,0 +1,11 @@
+package com.jinxin.demo01;
+
+/**
+ * @Author: JinXin
+ * @Date: 2018/3/11 16:12
+ */
+public class Demo01 {
+    public static void main(String[] args) {
+        System.out.println("helloworld");
+    }
+}
